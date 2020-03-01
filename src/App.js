857911af9faf28036b3b3322480ab4e1a49de8ab
Oleh Andreiev <oandreiev@lohika.com>
2020-03-01 20:10:19 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import User from './components/User'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  state = {
+    users: [
+      {
+        id: '1',
+        name: 'Alex',
+      },
+      {
+        id: '2',
+        name: 'Oleg',
+      },
+      {
+        id: '3',
+        name: 'Julia',
+      }
+    ],
+    name: ''
+  }
+
+  onUserRemove = id => {
+    const updatedUsers = this.state.users.filter(user => user.id !== id);
+    this.setState({ users: updatedUsers })
+  }
+
+
+  renderUsers() {
+    return this.state.users.map(user =>
+      <User
+        key={user.id} 
+        onUserRemove={this.onUserRemove}
+        data={user}
+      />
+    )
+  }
+
+  onInputChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  addUser = () => {
+    const { users, name } = this.state;
+    const updateUsers = [
+      ...users,
+      {
+        id: Math.random() + '',
+        name
+      }
+    ]
+    this.setState({
+      users: updateUsers,
+      name: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderUsers()}
+        <br />
+        <br />
+        Hello - <span>{this.state.name}</span>
+        <br />
+        <br />
+        <input placeholder="Some name" value={this.state.name} onChange={this.onInputChange} type="text" />
+        <button onClick={this.addUser}>Add user</button>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import classes from './User.module.css';
+
+
+class User extends Component {
+    render() {
+        const { name, id } = this.props.data;
+        return (
+            <div className={classes.user} style={styles}>
+                <div></div>
+                {name}
+                
+                <button className={styles.removeButton} onClick={() => this.props.onUserRemove(id)}>remove</button>
+            </div>
+        )
+    }
+}
+
+export  default User;
